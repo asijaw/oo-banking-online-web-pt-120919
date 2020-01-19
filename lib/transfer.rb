5408ec683ce1d419efbf44da065fc285b7f9c52f
Asija Watson <asija.w@yahoo.com>
2020-01-19 06:09:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -32,8 +32,9 @@ class Transfer
   end 
   
   def reverse_transfer
-    sender.balance += amount
-    receiver.balance -= amount
-    @status = "reversed"
+    if status == "complete"
+      sender.balance += amount
+      receiver.balance -= amount
+      @status = "reversed"
   end
 end
