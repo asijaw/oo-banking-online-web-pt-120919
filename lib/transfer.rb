60c6e226a3ae57d839245d4df1155f8220d6ed77
Asija Watson <asija.w@yahoo.com>
2020-01-19 05:40:00 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,10 @@
 class Transfer
   attr_accessor :transfer, :bank_account
   
-  def initialize
+  def initialize(transfer, bank_account, amount)
     @transfer = "pending"
+    @bank_account = bank_account
+    @amount = amount
+    
   end 
 end
